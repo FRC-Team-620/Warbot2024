8792a6ec5660f316bee28f449d1cd3780058c1d0
Andrei Stan <thomas.santonicola@gmail.com>
2025-02-15 13:31:56 -0500

    Upgraded to the 2025 lib.

--- src/main/java/org/jmhsrobotics/frc2024/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/org/jmhsrobotics/frc2024/subsystems/intake/IntakeSubsystem.java
@@ -6,25 +6,28 @@ import org.jmhsrobotics.frc2024.utils.SimTimeOfFlight;
 import org.jmhsrobotics.frc2024.utils.SimableTimeOfFlight;
 import org.jmhsrobotics.warcore.rev.RevEncoderSimWrapper;
 
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLimitSwitch;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import com.playingwithfusion.TimeOfFlight;
 import com.playingwithfusion.TimeOfFlight.RangingMode;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkLimitSwitch;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.Debouncer;
-import edu.wpi.first.math.system.plant.DCMotor;
+//import edu.wpi.first.math.system.plant.DCMotor; Commented out for code that was unable to upgrade
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj.simulation.DIOSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import monologue.Logged;
 
-public class IntakeSubsystem extends SubsystemBase implements Logged {
-	private CANSparkMax intakeMotor;
+public class IntakeSubsystem extends SubsystemBase {
+	private SparkMax intakeMotor;
+	private SparkMaxConfig intakeMotorConfig = new SparkMaxConfig();
 
 	private SimableTimeOfFlight lowerSensor;
 	private SimableTimeOfFlight upperSensor;
@@ -32,13 +35,19 @@ public class IntakeSubsystem extends SubsystemBase implements Logged {
 	private boolean isIntaking = false;
 
 	public IntakeSubsystem() {
-		intakeMotor = new CANSparkMax(Constants.CAN.kIntakeId, MotorType.kBrushless);
-		intakeMotor.setInverted(true);
-		intakeMotor.setIdleMode(IdleMode.kBrake);
+		// Create an instance of the intake
+		intakeMotor = new SparkMax(Constants.CAN.kIntakeId, MotorType.kBrushless);
+
+		// Set the default configuration for the intake
+		this.intakeMotorConfig.inverted(true);
+		this.intakeMotorConfig.idleMode(IdleMode.kBrake);
+		this.intakeMotorConfig.smartCurrentLimit(35);
+
+		// Apply the configuration
+		this.intakeMotor.configure(this.intakeMotorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
 
 		this.lowerSensor = new SimableTimeOfFlight(1);
 		this.upperSensor = new SimableTimeOfFlight(0);
-		intakeMotor.setSmartCurrentLimit(35);
 
 		this.lowerSensor.setRangingMode(RangingMode.Short, 24);
 		this.upperSensor.setRangingMode(RangingMode.Short, 24);
@@ -64,12 +73,6 @@ public class IntakeSubsystem extends SubsystemBase implements Logged {
 		// this.lowSwitch().isPressed());
 
 		SmartDashboard.putBoolean("intake/hasNote", this.hasNote());
-
-		log("intakeDutyCycle", intakeMotor.get());
-		log("hasNote", this.hasNote());
-		log("noteTooHigh", this.noteTooHigh());
-		log("lowerSensoDistance", this.lowerSensor.getRange());
-		log("upperSensor", this.upperSensor.getRange());
 	}
 
 	public void set(double speed) {
@@ -85,11 +88,11 @@ public class IntakeSubsystem extends SubsystemBase implements Logged {
 	}
 
 	public SparkLimitSwitch lowSwitch() {
-		return this.intakeMotor.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed);
+		return this.intakeMotor.getReverseLimitSwitch();
 	}
 
 	public SparkLimitSwitch highSwitch() {
-		return this.intakeMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed);
+		return this.intakeMotor.getForwardLimitSwitch();
 	}
 
 	public boolean hasNote() {
@@ -108,7 +111,8 @@ public class IntakeSubsystem extends SubsystemBase implements Logged {
 	private Debouncer intakeDebounceSim = new Debouncer(0.2);
 	public void simInit() {
 		intakeSwitchSim = new DIOSim(Constants.DIO.kIntakeSwitch);
-		intakeSim = new DCMotorSim(DCMotor.getNEO(1), 1, 0.3);
+		// Was unable to convert this to 2025, commenting out
+		//intakeSim = new DCMotorSim(DCMotor.getNEO(1), 1, 0.3);
 		intakeEncSim = RevEncoderSimWrapper.create(intakeMotor);
 		lowerSim = new SimTimeOfFlight(lowerSensor);
 		upperSim = new SimTimeOfFlight(upperSensor);
